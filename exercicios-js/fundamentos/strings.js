1239d54e8d86d96d8947f3f63b3598fc1df100fd
Edson Fernandes Cunha <52961166+efcunha@users.noreply.github.com>
2022-04-13 17:45:35 +0000

    curso update

--- exercicios-js/fundamentos/strings.js
+++ exercicios-js/fundamentos/strings.js
@@ -12,7 +12,6 @@ console.log(escola.substring((0, 3)))
 console.log('escola'.concat((escola).concat("!")))
 console.log('escola' + escola + "!")
 
-console.log(escola.replace((3, 'e')))
-console.log(escola.replace(((/\d/,'e'), 3)))
+console.log(escola.replace(3,`e`))
 
 console.log('Ana,Maria,Pedro'.split((',')))
--- exercicios-js/fundamentos/templateString.js
+++ exercicios-js/fundamentos/templateString.js
@@ -8,6 +8,4 @@ console.log(`1 + 1 = ${1 + 1}`)
 
 const up = texto => texto.toUpperCase()
 
-console.log('Ei... ${up('cuidado')}!')
-
-
+console.log(`Ei... ${up('cuidado')}!`)
