2c3190836746af2c39685532c904240ad573301f
Edson Fernandes Cunha <52961166+efcunha@users.noreply.github.com>
2022-04-13 16:18:31 +0000

    curso update

--- exercicios-js/fundamentos/numerosAlgunsCuidados.js
+++ exercicios-js/fundamentos/numerosAlgunsCuidados.js
@@ -1,5 +1,7 @@
 console.log(7 / 0) //Infinito
 console.log("10" / 2)
+console.log('3' + 2) //concatena a string ganha p valor do numero
+console.log('3' - 2)
 console.log("Shown!" * 2)
 console.log(0.1 + 0.7)
 //console.log(10.toString(()))
