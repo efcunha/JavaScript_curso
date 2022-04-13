93aa72db805779f9373c9a07bbeedae54b2cc353
Edson Fernandes Cunha <52961166+efcunha@users.noreply.github.com>
2022-04-13 12:02:23 +0000

    update curso

---
+++ exercicios-js/fundamentos/variaveisEConstantes.js
@@ -0,0 +1,19 @@
+// VAR (Identificador)
+var a = 3
+
+// Declaração de variavel mais moderna
+let b = 4
+
+var a = 30
+b = 40
+
+console.log(a, b);
+
+a = 300
+b = 400
+
+console.log(a, b);
+
+const c = 5
+// c = 50
+console.log(c);
