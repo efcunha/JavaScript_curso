2544d73f3d0a09e6d66cf9c71228c5a2a5df0060
Edson Fernandes Cunha <52961166+efcunha@users.noreply.github.com>
2022-04-20 17:30:39 +0000

    curso update

---
+++ exercicios-js/fundamentos/ternario.js
@@ -0,0 +1,3 @@
+const resultado = nota => nota >= 7 ? 'Aprovado' : 'Reprovado' // se nota for maior ou igual a 7 retorna aprovado senão reprovado
+console.log(resultado(7.1))
+console.log(resultado(6.7))
