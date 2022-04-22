d7b0a47cf7faaf31bdad6767cf0983ac3a80b97c
Edson Fernandes Cunha <52961166+efcunha@users.noreply.github.com>
2022-04-22 12:20:35 +0000

    curso update

---
+++ JS-FUNCTIONS/foundation/before.js
@@ -0,0 +1,15 @@
+// 1. Prerequisites
+
+// -> Basic knowledge of JS
+// ---> Data types: numbers, strings, booleans, arrays, objects
+// ---> Operators: +, -, *, /, %, ++, --, ==, ===, !=, !==, >, <, >=, <=, &&, ||, !
+// ---> If, For, While
+
+// 2. Node
+// -> NodeJS 
+
+// 3. Visual Studio Code
+// -> Code Runner
+// --> Code Runner: Clear Previous Output
+// --> Ctrl + Alt + n (run code)
+// --> Ctrl + Alt + m (stop the running code)
