2f730cafda4185fe5ea10af594ecfd387725cab2
Edson Fernandes Cunha <52961166+efcunha@users.noreply.github.com>
2022-05-02 20:29:52 +0000

    curso update

---
+++ exercicios-js/função/callback1.js
@@ -0,0 +1,11 @@
+const fabricantes = ["Mercedes", "Audi", "BMW"];
+
+function imprimir(nome, indice) {
+  console.log(`${indice + 1}. ${nome}`);
+}
+
+//fabricantes.forEach(imprimir)
+//fabricantes.forEach(fabricante => console.log(fabricante));
+fabricantes.forEach((fabricante, indice) => console.log(fabricante, indice));
+//fabricantes.forEach((fabricante, indice, array) => console.log(fabricante, indice, array));
+//fabricantes.forEach(fabricante => console.log(fabricante));
