9a2bfa7b510a2febb7a2a539e0597ec7bf5d474f
Edson Fernandes Cunha <52961166+efcunha@users.noreply.github.com>
2022-05-23 08:56:19 -0400

    curso update

---
+++ Spread Fullstack Developer/introdução-javascript/teste.js
@@ -0,0 +1,13 @@
+// var preco = 2;
+var desconto  = 0.2;
+const PRECO = 2; // Os valores de contant são imutaveis 
+
+var total = PRECO - desconto;
+
+// Função
+
+function soma(a, b) {
+  return a + b; //8
+}
+
+soma(3, 5);
