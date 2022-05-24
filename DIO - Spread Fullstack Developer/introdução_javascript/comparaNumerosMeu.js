a30fb840bf2e86ed67a9b76c65cdde96e642ff3d
Edson Fernandes Cunha <52961166+efcunha@users.noreply.github.com>
2022-05-24 15:35:33 -0400

    curso update

--- DIO - Spread Fullstack Developer/introdução_javascript/comparaNumerosMeu.js
+++ DIO - Spread Fullstack Developer/introdução_javascript/comparaNumerosMeu.js
@@ -3,7 +3,7 @@
 // Confira se a soma dos números é maior que 10 ou menor que 20.
 // Retorne uma string dizendo "Os números `num1` e `num2` não/são iguais. Sua soma é `soma`, que é `maior/menor` que 10 e `maior/menor` que 20".
 
-function verificaSoma(num1, num2) {
+function comparaNumeros(num1, num2) {
   if (num1 === num2) {
     return `Os números ${num1} e ${num2} são iguais. Sua soma é ${num1 + num2}, que é ${num1 + num2 > 10 ? "maior" : "menor"} que 10 e ${num1 + num2 <= 20 ? "menor" : "maior" } do que 20.`;
   } else if (num1 + num2 > 10 && num1 + num2 < 20) {
@@ -13,6 +13,6 @@ function verificaSoma(num1, num2) {
   }
 }
 
-console.log(verificaSoma(10, 10));
-console.log(verificaSoma(10, 20));
-console.log(verificaSoma(10, 5));
+console.log(comparaNumeros(10, 10));
+console.log(comparaNumeros(10, 20));
+console.log(comparaNumeros(10, 5));
