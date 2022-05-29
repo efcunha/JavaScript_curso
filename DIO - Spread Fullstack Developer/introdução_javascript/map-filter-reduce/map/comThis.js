33d82b4ca4c5420609fab28a91ba9420b2034a9a
Edson Fernandes Cunha <52961166+efcunha@users.noreply.github.com>
2022-05-29 13:41:52 -0400

    commit

--- DIO - Spread Fullstack Developer/introdução_javascript/map-filter-reduce/map/comThis.js
+++ DIO - Spread Fullstack Developer/introdução_javascript/map-filter-reduce/map/comThis.js
@@ -1,17 +1,17 @@
 const orange = {
-	price: 2,
+  price: 2,
 };
 
 const apple = {
-	price: 1.5,
+  price: 1.5,
 };
 
 function mapArray() {
-	const array = [1, 2, 3, 4, 5];
+  const array = [1, 2, 3, 4, 5];
 
-	return array.map(function (item) {
-		return item * this.price;
-	}, apple);
+  return array.map(function (item) {
+    return item * this.price;
+  }, apple);
 }
 
 console.log(mapArray());
